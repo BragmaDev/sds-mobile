309c429435ad4034fa892a5ddaf7f3d1f788727a
Bragma <mh.bragge@gmail.com>
2021-11-30 03:10:31 +0200

    added and completed the list app project

---
+++ Coursework/ListApp/app/src/main/java/com/example/listapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.listapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView my_list_view;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        my_list_view = (ListView)findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter item_adapter = new ItemAdapter(this, items, prices, descriptions);
+        my_list_view.setAdapter(item_adapter);
+
+        my_list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent show_detail_activity = new Intent(getApplicationContext(), DetailActivity.class);
+                show_detail_activity.putExtra("com.example.ITEM_INDEX", i);
+                startActivity(show_detail_activity);
+            }
+        });
+    }
+}
